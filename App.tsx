355b21c35e699f250f5b1d0e1444a9e70eb5fc24
Wilson Junior <wilson.junior@merx.tech>
2022-06-28 22:29:25 -0300

    feat: add splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+import { loadAsync } from 'expo-font';
 import { 
-  useFonts,
   Inter_400Regular,
   Inter_500Medium
 } from '@expo-google-fonts/inter';
@@ -12,26 +13,56 @@ import {
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo';
 
-import { Home } from './src/screens/Home';
+import { CarDetails } from './src/screens/CarDetails';
 
 import theme from './src/styles/theme';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Archivo_400Regular,
-    Archivo_500Medium,
-    Archivo_600SemiBold
-  })
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (!fontsLoaded) {
-    return <AppLoading />
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+        await loadAsync({ 
+          Inter_400Regular,
+          Inter_500Medium,
+          Archivo_400Regular,
+          Archivo_500Medium,
+          Archivo_600SemiBold,
+        });
+     
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
+
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <View
+        style={{ flex: 1 }}
+        onLayout={onLayoutRootView}
+      >
+        <CarDetails />
+      </View>
     </ThemeProvider>
   );
 }
