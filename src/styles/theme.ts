3548b2565d9387071db3cd35759279957564e9ad
Wilson Junior <wilson.junior@merx.tech>
2022-07-12 23:40:14 -0300

    fix: lint

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -19,11 +19,11 @@ export default {
     shape_dark: '#29292E',
   },
   fonts: {
-    primary_400: "Inter_400Regular",
-    primary_500: "Inter_500Medium",
+    primary_400: 'Inter_400Regular',
+    primary_500: 'Inter_500Medium',
 
-    secondary_400: "Archivo_400Regular",
-    secondary_500: "Archivo_500Medium",
-    secondary_600: "Archivo_600SemiBold",
+    secondary_400: 'Archivo_400Regular',
+    secondary_500: 'Archivo_500Medium',
+    secondary_600: 'Archivo_600SemiBold',
   },
-}
+};
